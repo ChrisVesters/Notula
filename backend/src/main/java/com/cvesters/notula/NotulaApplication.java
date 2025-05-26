22a968d33d728e8d3421074d2412be437e87b964
Chris Vesters <vesters.chris@outlook.com>
2025-05-26 16:53:07 +0200

    Setup backend

---
+++ backend/src/main/java/com/cvesters/notula/NotulaApplication.java
@@ -0,0 +1,13 @@
+package com.cvesters.notula;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotulaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotulaApplication.class, args);
+	}
+
+}
