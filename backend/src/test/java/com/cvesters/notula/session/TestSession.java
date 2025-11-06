54e43c21a9c02dcb102b0452c4b0425483c4ab4f
Chris Vesters <vesters.chris@outlook.com>
2025-11-06 13:58:38 +0100

    3 - Add session repository

---
+++ backend/src/test/java/com/cvesters/notula/session/TestSession.java
@@ -0,0 +1,36 @@
+package com.cvesters.notula.session;
+
+import java.time.OffsetDateTime;
+import java.time.Period;
+
+import lombok.Getter;
+
+import com.cvesters.notula.session.bdo.SessionInfo;
+import com.cvesters.notula.user.TestUser;
+
+@Getter
+public enum TestSession {
+	EDUARDO_CHRISTIANSEN_DEKSTOP(1L, TestUser.EDUARDO_CHRISTIANSEN, "abc7775",
+			OffsetDateTime.now().plus(Period.ofDays(7))),
+	JUDY_HARBER_MOBILE(2L, TestUser.JUDY_HARBER, "eff74def",
+			OffsetDateTime.now().plus(Period.ofDays(30))),
+	EDUARDO_CHRISTIANSEN_MOBILE(7L, TestUser.EDUARDO_CHRISTIANSEN, "ddef741",
+			OffsetDateTime.now().minus(Period.ofDays(1)));
+
+	private final long id;
+	private final TestUser user;
+	private final String refreshToken;
+	private final OffsetDateTime activeUntil;
+
+	TestSession(final long id, final TestUser user, final String refreshToken,
+			final OffsetDateTime activeUntil) {
+		this.id = id;
+		this.user = user;
+		this.refreshToken = refreshToken;
+		this.activeUntil = activeUntil;
+	}
+
+	public SessionInfo info() {
+		return new SessionInfo(id, user.getId(), refreshToken, activeUntil);
+	}
+}
