ae716e1b55b38fcd51640c015e5b7c2a2ae52a07
Chris Vesters <vesters.chris@outlook.com>
2025-12-27 08:09:44 +0100

    8 - Select organisations - db

--- backend/src/main/java/com/cvesters/notula/organisation/OrganisationRepository.java
+++ backend/src/main/java/com/cvesters/notula/organisation/OrganisationRepository.java
@@ -1,10 +1,14 @@
 package com.cvesters.notula.organisation;
 
+import java.util.List;
+
 import org.springframework.data.repository.Repository;
 
 import com.cvesters.notula.organisation.dao.OrganisationDao;
 
 public interface OrganisationRepository extends Repository<OrganisationDao, Long> {
+
+	List<OrganisationDao> findAllById(final Iterable<Long> ids);
 
 	OrganisationDao save(final OrganisationDao organisation);
 
--- backend/src/test/java/com/cvesters/notula/organisation/OrganisationUserRepositoryTest.java
+++ backend/src/test/java/com/cvesters/notula/organisation/OrganisationUserRepositoryTest.java
@@ -25,13 +25,66 @@ public class OrganisationUserRepositoryTest extends RepositoryTest {
 	private OrganisationUserRepository organisationUserRepository;
 
 	@Nested
+	class FindAllByUserId {
+
+		@Test
+		void single() {
+			final var userId = TestUser.EDUARDO_CHRISTIANSEN.getId();
+
+			final var result = organisationUserRepository
+					.findAllByUserId(userId);
+
+			assertThat(result).hasSize(1).anySatisfy(orgUser -> {
+				assertThat(orgUser.getId()).isEqualTo(
+						TestOrganisationUser.SPORER_EDUARDO_CHRISTIANSEN
+								.getId());
+				assertThat(orgUser.getOrganisationId())
+						.isEqualTo(TestOrganisation.SPORER.getId());
+				assertThat(orgUser.getUserId()).isEqualTo(userId);
+			});
+		}
+
+		@Test
+		void multiple() {
+			final var userId = TestUser.ALISON_DACH.getId();
+
+			final var result = organisationUserRepository
+					.findAllByUserId(userId);
+
+			assertThat(result).hasSize(2).anySatisfy(orgUser -> {
+				assertThat(orgUser.getId()).isEqualTo(
+						TestOrganisationUser.GLOVER_ALISON_DACH.getId());
+				assertThat(orgUser.getOrganisationId())
+						.isEqualTo(TestOrganisation.GLOVER.getId());
+				assertThat(orgUser.getUserId()).isEqualTo(userId);
+			}).anySatisfy(orgUser -> {
+				assertThat(orgUser.getId()).isEqualTo(
+						TestOrganisationUser.HEUL_ALISON_DACH.getId());
+				assertThat(orgUser.getOrganisationId())
+						.isEqualTo(TestOrganisation.HEUL.getId());
+				assertThat(orgUser.getUserId()).isEqualTo(userId);
+			});
+		}
+
+		@Test
+		void none() {
+			final var userId = Long.MAX_VALUE;
+
+			final var organisationUsers = organisationUserRepository
+					.findAllByUserId(userId);
+
+			assertThat(organisationUsers).isEmpty();
+		}
+	}
+
+	@Nested
 	class Save {
 
 		@Test
 		void success() {
-			final TestUser USER = TestUser.JUDY_HARBER;
+			final var user = TestUser.DAPHNEE_LESCH;
 			final var bdo = new OrganisationUserInfo(ORGANISATION.getId(),
-					USER.getId());
+					user.getId());
 			final var dao = new OrganisationUserDao(bdo);
 
 			final OrganisationUserDao saved = organisationUserRepository
@@ -39,7 +92,7 @@ public class OrganisationUserRepositoryTest extends RepositoryTest {
 
 			assertThat(saved.getOrganisationId())
 					.isEqualTo(ORGANISATION.getId());
-			assertThat(saved.getUserId()).isEqualTo(USER.getId());
+			assertThat(saved.getUserId()).isEqualTo(user.getId());
 
 			final OrganisationUserDao found = entityManager
 					.find(OrganisationUserDao.class, saved.getId());
@@ -47,7 +100,7 @@ public class OrganisationUserRepositoryTest extends RepositoryTest {
 			assertThat(found.getId()).isEqualTo(saved.getId());
 			assertThat(found.getOrganisationId())
 					.isEqualTo(ORGANISATION.getId());
-			assertThat(found.getUserId()).isEqualTo(USER.getId());
+			assertThat(found.getUserId()).isEqualTo(user.getId());
 		}
 
 		@Test
