ab868ec2d47e398b8705f6a26d11c36bb145e88e
Chris Vesters <vesters.chris@outlook.com>
2026-05-31 07:27:05 +0200

    32 - Add front-end logic to update meeting description.
    Closes #32

--- frontend/src/lib/meeting/MeetingInfoView.svelte
+++ frontend/src/lib/meeting/MeetingInfoView.svelte
@@ -5,7 +5,8 @@
 	import type { UpdateAction } from "$lib/editor/ActionTypes";
 
 	import Input from "$lib/editor/Input.svelte";
-	import type { MeetingUpdateNameAction } from "./MeetingTypes";
+	import TextArea from "$lib/editor/TextArea.svelte";
+	import type { MeetingUpdateDescriptionAction, MeetingUpdateNameAction } from "./MeetingTypes";
 	import MeetingWebSocketClient from "./MeetingWebSocketClient";
 
 	export type MeetingInfoViewProps = {
@@ -24,6 +25,17 @@
 
 		MeetingWebSocketClient.updateName(meeting.id, request);
 	};
+
+	const handleUpdateDescription = (action: UpdateAction) => {
+		const request: MeetingUpdateDescriptionAction = {
+			action: "UPDATE_DESCRIPTION",
+			position: action.position,
+			length: action.length,
+			value: action.value
+		};
+
+		MeetingWebSocketClient.updateDescription(meeting.id, request);
+	};
 </script>
 
 <Input
@@ -31,4 +43,10 @@
 	bind:value={meeting.name}
 	placeholder={$t("common.untitled")}
 	onAction={handleUpdateName}
+/>
+
+<TextArea
+	bind:value={meeting.description}
+	placeholder={$t("common.startTyping")}
+	onAction={handleUpdateDescription}
 />
