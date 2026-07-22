d20e72c7d4d6acad661862b383760d97a9beef26
Chris Vesters <vesters.chris@outlook.com>
2026-07-22 15:07:23 +0200

    66 - Add role to front-end.

--- frontend/src/lib/organisation/OrganisationUserTypes.ts
+++ frontend/src/lib/organisation/OrganisationUserTypes.ts
@@ -3,14 +3,25 @@ export type OrganisationUserView = {
 	organisationId: number;
 	userId: number;
 	email: string;
+	role: OrganisationUserRole;
 };
 
 export type OrganisationUserInfo = {
 	id: number;
 	organisationId: number;
 	userId: number;
+	role: OrganisationUserRole;
 };
 
 export type OrganisationUserCreateRequest = {
 	email: string;
+	role: OrganisationUserRole;
 };
+
+export const OrganisationUserRole = {
+	ADMIN: "ADMIN",
+	MEMBER: "MEMBER"
+} as const;
+
+export type OrganisationUserRole =
+	(typeof OrganisationUserRole)[keyof typeof OrganisationUserRole];
