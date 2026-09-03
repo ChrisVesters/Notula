807f9229a43fa6149f223a7cf1fe72a6923fb061
Chris Vesters <vesters.chris@outlook.com>
2026-09-03 09:44:37 +0200

    94 - Add a dedicated thread pool for weboscket mesages.

--- backend/src/main/java/com/cvesters/notula/config/Config.java
+++ backend/src/main/java/com/cvesters/notula/config/Config.java
@@ -3,6 +3,7 @@ package com.cvesters.notula.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -13,6 +14,15 @@ public class Config {
 	@Bean
 	PasswordEncoder passwordEncoder() {
 		return Argon2PasswordEncoder.defaultsForSpringSecurity_v5_8();
+	}
+
+	@Bean
+	ThreadPoolTaskExecutor webSocketInboundExecutor() {
+		final var executor = new ThreadPoolTaskExecutor();
+		final int cores = Runtime.getRuntime().availableProcessors();
+		executor.setCorePoolSize(cores);
+		executor.setThreadNamePrefix("ws-inbound-");
+		return executor;
 	}
 
 	@Bean
